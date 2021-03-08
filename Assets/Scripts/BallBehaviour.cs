5de4412c2b6120ed06f27e463747484029d46cdc
Cara <co884@nyu.edu>
2021-03-08 00:11:36 -0500

    Committing treasure game code for our homework.

---
+++ Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour {
+
+public SpriteRenderer myRenderer;
+public Color floorColour;
+public Color gateColour;
+public Color yellowColour;
+public Color blueColour;
+public Color hinge2Colour;
+public Color gateColour2;
+public Color greenColour;
+public Color purpleColour;
+public Rigidbody2D myBody;
+
+public GameObject firstCam;
+float MoveDirection = 1;
+public float speed; 
+public float jumpHeight;
+public float GravityMultiplier;
+public float jumpMultiplier;
+//boolean to check if player is on floor/platform or not
+bool onFloor = true;
+
+public GameObject orangegate;
+public GameObject greengate;
+public GameObject redgate;
+public GameObject reddoor1;
+public GameObject reddoor2;
+
+
+
+
+
+public float power;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer = gameObject.GetComponent<SpriteRenderer>();
+        myBody = gameObject.GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //returns true once
+        
+    }
+
+    void FixedUpdate() {
+        //physics code runs in fixed update instead of update!
+
+        //make on floor false if our velocity is at a certain rate
+        if(onFloor && myBody.velocity.y > 0) {
+            onFloor = false; 
+        }
+        CheckKeys();
+        HandleMovement();
+        JumpPhysics();
+
+
+    }
+
+    void CheckKeys() {
+
+        if (Input.GetKey(KeyCode.D)) {
+
+            MoveDirection = 1;
+        }
+
+        else if (Input.GetKey(KeyCode.A)) {
+
+            MoveDirection = -1;
+        }
+
+        else {
+
+            MoveDirection = 0;
+        }
+
+        if (Input.GetKey(KeyCode.W) && onFloor) {
+
+            //we want to keep the x velocity as is
+            //but change y value thru jump height
+            myBody.velocity = new Vector3(myBody.velocity.x, jumpHeight);
+            //if we are not pressing W or on the floor 
+        } else if(!Input.GetKey(KeyCode.W) && myBody.velocity.y > 0) {
+            //little boost, then slow down 
+            myBody.velocity += Vector2.up * Physics.gravity.y * (jumpMultiplier - 1f) * Time.deltaTime; 
+        }
+
+    }
+
+    void JumpPhysics() {
+        //if we are in descent
+        if(myBody.velocity.y < 0) {
+            myBody.velocity += Vector2.up * Physics.gravity.y * (GravityMultiplier - 1f) * Time.deltaTime;
+        }
+    }
+
+    void HandleMovement() {
+
+        //only messing w/ x values
+        myBody.velocity = new Vector3(MoveDirection * speed, myBody.velocity.y);
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collisionInfo) {
+
+        //tag for group of objects
+        if(collisionInfo.gameObject.tag == "Floor") {
+
+            onFloor = true;
+        }
+        
+        
+
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.name == "orangemushroom") {
+            //myRenderer.color = gateColour; 
+            Debug.Log("picked up orange mushroom");
+            Destroy(other.gameObject);
+            Destroy(orangegate);
+        }
+
+        if (other.gameObject.name == "redmushroom") {
+            //myRenderer.color = gateColour; 
+            Debug.Log("picked up red mushroom");
+            Destroy(other.gameObject);
+            Destroy(reddoor2);
+            Destroy(redgate);
+        }
+
+         if (other.gameObject.name == "greenmushroom") {
+            //myRenderer.color = gateColour; 
+            Debug.Log("picked up green mushroom");
+            Destroy(other.gameObject);
+            Destroy(greengate);
+        }
+
+        if (other.gameObject.name == "redkey") {
+            //myRenderer.color = gateColour; 
+            Debug.Log("picked up green mushroom");
+            Destroy(other.gameObject);
+            Destroy(reddoor1);
+        }
+
+        
+    }
+    }
+    
+
+
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+
+{
+
+    public Transform followTransform;
+    public BoxCollider2D worldBounds;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    //camera positions
+    float camX;
+    float camY;
+    //width of camera
+    float camRatio;
+    //height of camera
+    float camSize;
+
+    public Camera mainCam;
+    //public Camera2 secondCam;
+
+    Vector3 smoothPos;
+
+    //speed (how quick the camera catches up to player)
+    public float smoothRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xMin = worldBounds.bounds.min.x;
+        xMax = worldBounds.bounds.max.x;
+        yMin = worldBounds.bounds.min.y;
+        yMax = worldBounds.bounds.max.y;
+
+        mainCam = gameObject.GetComponent<Camera>();
+        //secondCam = gameObject.GetComponent<Camera2>();
+
+        camSize = mainCam.orthographicSize;
+        //makes sure ball is in the center
+        camRatio = (xMax + camSize) / 8.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        //code from class
+        //called every fixed frame rate frame
+        //helps keep physics as stable as possible
+        camY = Mathf.Clamp(followTransform.position.y, yMin + camSize, yMax - camSize);
+        camX = Mathf.Clamp(followTransform.position.x, xMin + camRatio, xMax - camRatio);
+
+        smoothPos = Vector3.Lerp(gameObject.transform.position, new Vector3(camX, camY, gameObject.transform.position.z), smoothRate);
+
+        gameObject.transform.position = smoothPos;
+    }
+}
